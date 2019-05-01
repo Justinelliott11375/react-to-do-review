c8ea7f0de69f5bcb2a2e45482713fabbe69f845c
Justin Elliott <Justinelliott11375@gmail.com>
2019-05-01 17:56:52 -0500

    checkbox toggle and mark complete working

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       {
         id: 2,
         title: "Do something else",
-        completed: true
+        completed: false
       },
       {
         id: 3,
@@ -23,10 +23,21 @@ class App extends Component {
       }
     ]
   }
+
+  // toggle todo complete
+  markComplete = (id) => {
+    this.setState( { todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+
+      return todo;
+    }) });
+  }
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
       </div>
     );
   }
