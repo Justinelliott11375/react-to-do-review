35c622de53747951642a04352da6539de6e4cd4d
Justin Elliott <Justinelliott11375@gmail.com>
2019-05-01 21:07:12 -0500

    delete todo working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
+import uuid from 'uuid';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +10,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "Do something",
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "Do something else",
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "Do something else again",
         completed: false
       }
@@ -34,10 +37,30 @@ class App extends Component {
       return todo;
     }) });
   }
+
+  // delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
+  // add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    };
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }
